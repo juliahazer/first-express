95d790ae089770fd31b12ac3007179c4338eca77
Julia Hazer <juliahazer@yahoo.com>
2017-03-16 14:28:40 -0700

    modified landing page and added basic navigation

--- index.js
+++ index.js
@@ -10,20 +10,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 //app.use(bodyParser.json());
 
-var todos = ['eat','sleep','build a crud app', 'play tennis', 'learn React'];
+var todos = ['eat','sleep','build a CRUD app', 'play tennis'];
 
-var instructor = "Elie";
+var header = "Calculator";
 
 var first = "Julia";
 
 var dupMsg = "";
 
+app.get('/', function(req,res){
+	res.render('home');
+});
+
+app.get('/calculator', function(req,res){
+	res.render('calculator', 
+		{header: header}); 
+});
+
 //root route + a callback function - don't do until the user goes to the route
 //the callback function gets request & response parameters
-app.get('/', function(req, res){
-	res.render('hello', 
-		{instructorName: instructor}); //'hello.ejs' is optional since set view engine to be ejs
-}); //sending variable to the template
+/*app.get('/', function(req, res){
+	res.render('calculator', 
+		{header: header}); //'hello.ejs' is optional since set view engine to be ejs
+}); //sending variable to the template*/
 
 app.get('/todos', function(req,res){
 	res.render('todo', 
@@ -84,17 +93,17 @@ app.get('/login', function(req, res){
 
 //ok to duplicate with the above name because 1st is a get and the 2nd is a post
 app.post('/login', function (req, res){
-	res.send(req.body);
-	//res.redirect('/welcome');
+	//res.send(req.body);
+	res.redirect('/welcome');
 });
 
 app.get('/welcome', function(req, res){
-	res.send('Logged In!');
+	res.send('<h1>Logged In!</h1><a href="./">Back to homepage</a>');
 });
 
 function notNumbers(num1, num2){
 	if (isNaN(num1) || isNaN(num2)){
-		return `Please enter 2 numbers!`;
+		return `<h1>Error</h1>Please enter 2 numbers!`;
 	}
 	return ""
 }
@@ -124,12 +133,13 @@ app.get('/calculate', (req,res) => {
 			 operSymbol = "*";	
 		}
 
-		message = `${num1} ${operSymbol} ${num2} = ${value}`;
+		message = `<h1>Answer:</h1>${num1} ${operSymbol} ${num2} = ${value}`;
 	}
+	message += `<br><br><a href="calculator">Back</a>`;
 	res.send(message);
 });
 
-app.get('/add', function(req,res){
+/*app.get('/add', function(req,res){
 	var num1 = Number(req.query.num1);
 	var num2 = Number(req.query.num2);
 	var message = notNumbers(num1,num2);
@@ -163,7 +173,7 @@ app.get('/divide/:num1/:num2', function(req,res){
 	var num2 = Number(req.params.num2);
 	var divisor = num1 / num2;
 	res.send(`The divisor of ${num1} / ${num2} = ${divisor}`);
-});
+});*/
 
 //STARTS THE SERVER - ctrl + c to stop the server
 //tell application to start a server -- tell app to listen on a port
